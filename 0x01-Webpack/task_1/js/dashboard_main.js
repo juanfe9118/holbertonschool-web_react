9be261bda6f3426846c13bfa8e5b151649485115
juanfe9118 <1168@holbertonschool.com>
2021-02-11 17:38:22 -0500

    Go back to one paragraph per line

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -1,7 +1,7 @@
-import $ from "jquery";
-import { debounce } from "lodash";
+import $ from 'jquery';
+import { debounce } from 'lodash';
 
-$('body').append("<p>Hoberton Dashboard</p><p>Dashboard data for the students</p><button>Click here to get started</button><p id='count'></p><p>Copyright - Holberton School</p>");
+$('body').append("<p>Holberton Dashboard</p><p>Dashboard data for the students</p><button>Click here to get started</button><p id='count'></p><p>Copyright - Holberton School</p>");
 
 let count = 0;
 
