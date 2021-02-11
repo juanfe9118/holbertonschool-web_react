38066b506779c7a8be13c7d2a01a7e567ae7bde5
juanfe9118 <1168@holbertonschool.com>
2021-02-11 14:30:29 -0500

    Change jquery selector

--- 0x01-Webpack/task_0/src/index.js
+++ 0x01-Webpack/task_0/src/index.js
@@ -1,3 +1,3 @@
 import $ from "jquery";
 
-$('body').append('<p>Hoberton Dashboard</p><p>Dashboard data for the students</p><p>Copyright - Holberton School</p>');
+$(body).append('<p>Hoberton Dashboard</p><p>Dashboard data for the students</p><p>Copyright - Holberton School</p>');
