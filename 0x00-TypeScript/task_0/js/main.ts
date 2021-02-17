e4e5c312f8d50c7aa28c517ea86390ed947ac9e2
juanfe9118 <1168@holbertonschool.com>
2021-02-17 14:05:49 -0500

    Update task

--- 0x00-TypeScript/task_0/js/main.ts
+++ 0x00-TypeScript/task_0/js/main.ts
@@ -19,4 +19,32 @@ const std2: Student = {
     location: 'Orlando, FL'
 };
 
-const studentsList = [std1, std2];
+const studentsList: Array<Student> = [std1, std2];
+
+const body: HTMLBodyElement = document.getElementsByTagName("body")[0];
+const table: HTMLTableElement = document.createElement('table');
+const thead: HTMLTableSectionElement = document.createElement('thead');
+const tbody: HTMLTableSectionElement = document.createElement('tbody');
+const tr: HTMLTableRowElement = document.createElement('tr');
+const th1: HTMLTableCellElement = document.createElement('td');
+const th2: HTMLTableCellElement = document.createElement('td');
+
+body.append(table);
+table.append(thead);
+table.append(tbody)
+thead.append(tr);
+tr.append(th1);
+tr.append(th2);
+
+studentsList.forEach(element => {
+  const trTb: HTMLTableRowElement = document.createElement('tr');
+  tbody.append(trTb);
+
+  let td: HTMLTableCellElement = document.createElement('td');
+  td.innerHTML = element.firstName;
+  trTb.append(td);
+
+  td = document.createElement('td');
+  td.innerHTML = element.location;
+  trTb.append(td);
+});
