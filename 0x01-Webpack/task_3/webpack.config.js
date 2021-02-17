243746fdb4b6d18c750cbf50edb85c2a83ffec4c
juanfe9118 <1168@holbertonschool.com>
2021-02-17 12:22:44 -0500

    Modify config file

--- 0x01-Webpack/task_3/webpack.config.js
+++ 0x01-Webpack/task_3/webpack.config.js
@@ -4,9 +4,11 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
 entry: {
+all: {
 header: "./modules/header/header.js",
 body: "./modules/body/body.js", 
 footer: "./modules/footer/footer.js"
+}
 },
 mode: 'development',
 performance: {
@@ -14,38 +16,38 @@ maxAssetSize: 1000000,
 maxEntrypointSize: 1000000
 },
 output: {
-  filename: "[name]bundle.js",
-  path: path.resolve(__dirname, "public")
+filename: "[name]bundle.js",
+path: path.resolve(__dirname, "public")
 },
 devtool: 'inline-source-map',
 devServer: {
-  contentBase: path.join(__dirname, './public'),
-  compress: true,
-  port: 8564,
+contentBase: path.join(__dirname, './public'),
+compress: true,
+port: 8564,
 },
 plugins: [
-  new CleanWebpackPlugin(),
-  new HtmlWebpackPlugin(),
+new CleanWebpackPlugin(),
+new HtmlWebpackPlugin(),
 ],
 module: {
 rules: [
-  {
-    test: /\.css$/i,
-    use: ['style-loader', 'css-loader']
-  },
-  {
-    test: /\.(gif|png|svg|jpe?g)$/i,
-    use: [
-      'file-loader',
-      {
-        loader: 'image-webpack-loader',
-        options: {
-          bypassOnDebug: true,
-          disable: true
-        }
+{
+  test: /\.css$/i,
+  use: ['style-loader', 'css-loader']
+},
+{
+  test: /\.(gif|png|svg|jpe?g)$/i,
+  use: [
+    'file-loader',
+    {
+      loader: 'image-webpack-loader',
+      options: {
+        bypassOnDebug: true,
+        disable: true
       }
-    ]
-  }
+    }
+  ]
+}
 ]
 }
 }
